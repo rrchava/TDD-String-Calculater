42afd378bc1829c2832a3fbc76360e6b44a17893
Ramarao <ramarao.chava@ratnaglobaltech.com>
2024-08-06 12:08:43 +0530

    updated implementation for new line seperater

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,7 +2,7 @@
 export class StringCalculator {
     add(numbers: string): number {
       if (numbers === "") return 0;
-      const numArray = numbers.split(",").map(Number);
+      const numArray = numbers.split(/[\n,]+/).map(Number);
       return numArray.reduce((a, b) => a + b, 0);
     }
   }
