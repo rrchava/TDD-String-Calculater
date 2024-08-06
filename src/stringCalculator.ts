6924d5148d78f8891adc8b5aa1624d691bdc9d37
Ramarao <ramarao.chava@ratnaglobaltech.com>
2024-08-06 12:13:08 +0530

    updated implemenation for multiple delimiter

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,7 +2,15 @@
 export class StringCalculator {
     add(numbers: string): number {
       if (numbers === "") return 0;
-      const numArray = numbers.split(/[\n,]+/).map(Number);
+  
+      let delimiter = /[\n,]+/;
+      if (numbers.startsWith("//")) {
+        const parts = numbers.split("\n", 2);
+        delimiter = new RegExp(parts[0][2]);
+        numbers = parts[1];
+      }
+  
+      const numArray = numbers.split(delimiter).map(Number);
       return numArray.reduce((a, b) => a + b, 0);
     }
   }
