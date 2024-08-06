acd936673fe1407bbcd86a5010e13525f97bddcb
Ramarao <ramarao.chava@ratnaglobaltech.com>
2024-08-06 12:06:29 +0530

    updated test case for new line between numbers

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -20,5 +20,10 @@ describe('StringCalculator', () => {
     const calculator = new StringCalculator();
     expect(calculator.add("1,2,3,4")).toBe(10);
   });
+
+  it('should handle new lines between numbers', () => {
+    const calculator = new StringCalculator();
+    expect(calculator.add("1\n2,3")).toBe(6);
+  });
   
 });
