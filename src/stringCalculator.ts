124501e2f1caf8b6088fd58d311a9404745a14a7
Ramarao <ramarao.chava@ratnaglobaltech.com>
2024-08-06 11:57:15 +0530

    Implement add method for single number case

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,7 +2,7 @@
 export class StringCalculator {
     add(numbers: string): number {
       if (numbers === "") return 0;
+      if (!numbers.includes(",")) return parseInt(numbers);
       return 0;
     }
   }
-  
