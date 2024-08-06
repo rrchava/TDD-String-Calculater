828eea99353c67c51a0e2c4d964f17cb1bbbbb45
Ramarao <ramarao.chava@ratnaglobaltech.com>
2024-08-06 12:04:24 +0530

    updated implentation for multiple numbers

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,7 +2,6 @@
 export class StringCalculator {
     add(numbers: string): number {
       if (numbers === "") return 0;
-      if (!numbers.includes(",")) return parseInt(numbers);
       const numArray = numbers.split(",").map(Number);
       return numArray.reduce((a, b) => a + b, 0);
     }
