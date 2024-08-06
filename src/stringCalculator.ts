a01d1d5cc34c97d2f619553cba948824e55b63a2
Ramarao <ramarao.chava@ratnaglobaltech.com>
2024-08-06 12:00:42 +0530

    add implementation for two numbers

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -3,6 +3,7 @@ export class StringCalculator {
     add(numbers: string): number {
       if (numbers === "") return 0;
       if (!numbers.includes(",")) return parseInt(numbers);
-      return 0;
+      const numArray = numbers.split(",").map(Number);
+      return numArray.reduce((a, b) => a + b, 0);
     }
   }
